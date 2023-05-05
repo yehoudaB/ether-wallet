8a2e42edd3e4f91a5ca621173f04e016d5f8bb46
yehoudaB <yehouda.bouskila@external.totalenergies.com>
2023-05-05 15:07:16 +0200

    get balance with alchemy

--- src/app/services/contract.service.ts
+++ src/app/services/contract.service.ts
@@ -3,6 +3,7 @@ import {  ethers } from 'ethers';
 import  EtherWallet  from '../../artifacts/contracts/EtherWallet.sol/EtherWallet.json';
 import { BehaviorSubject } from 'rxjs';
 import { WalletService } from './wallet.service';
+import { AlchemyService } from './alchemy.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,46 +15,51 @@ export class ContractService {
 
   private responseSubject = new BehaviorSubject<any>('');
   response$= this.responseSubject.asObservable();
-
-  constructor(private  walletService: WalletService ) {
-     this.getContractBallance();
+  provider = new ethers.providers.Web3Provider(window.ethereum);
+  signer = this.provider.getSigner();
+  contract = new ethers.Contract(this.contractAddress, EtherWallet.abi, this.signer);
+  
+  constructor(
+    private  walletService: WalletService,
+    private alchemyService: AlchemyService ) {
+     this.getContractBallanceWithAlchemy();
    }
 
 
 
-  async getContractBallance() {
+  async getContractBallanceWithEther() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const contract = new ethers.Contract(this.contractAddress, EtherWallet.abi, provider);
 
     this.ethBalanceSubject.next(ethers.utils.formatEther(await contract['balanceOf']()));
+  }   
+  async getContractBallanceWithAlchemy() {
+    const balance = await this.alchemyService.alchemy.core.getBalance(this.contractAddress);
+    this.ethBalanceSubject.next(ethers.utils.formatEther( balance));
   }    
 
 
   async deposit(amount: number)  {
     try{
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(this.contractAddress, EtherWallet.abi, signer);
-      const transaction = await contract['deposit']({value: ethers.utils.parseEther(amount.toString())});
+      
+      const transaction = await this.contract['deposit']({value: ethers.utils.parseEther(amount.toString())});
       await transaction.wait();
-      await this.walletService.connectWalletWithEthers();
-      await this.getContractBallance();
+      await this.walletService.connectWalletWithAlchemy();
+      await this.getContractBallanceWithAlchemy();
       this.responseSubject.next( {reason: 'Deposit successful'});
     } catch (error) {
       console.log(error);
       this.responseSubject.next(error);
     }
   }   
-
+ 
   async withdraw(amount: number)  {
     try{
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(this.contractAddress, EtherWallet.abi, signer);
-      const transaction = await contract['withdraw']( signer.getAddress(), ethers.utils.parseEther(amount.toString()));
+      
+      const transaction = await this.contract['withdraw']( this.signer.getAddress(), ethers.utils.parseEther(amount.toString()));
       await transaction.wait();
-      await this.walletService.connectWalletWithEthers();
-      await this.getContractBallance();
+      await this.walletService.connectWalletWithAlchemy();
+      await this.getContractBallanceWithAlchemy();
       this.responseSubject.next( {reason: 'successful withdrawal'});
     } catch (error) {
       console.log(error);
--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import {  ethers } from 'ethers';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
-import { WalletData } from '../models/WalletData';
-import { Alchemy, AssetTransfersCategory, Network, OwnedNftsResponse, TokenBalancesResponseErc20 } from 'alchemy-sdk';
-import { Observable, from, map } from 'rxjs';
+import { OwnedNftsResponse, TokenBalancesResponseErc20 } from 'alchemy-sdk';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,67 +11,15 @@ export class WalletService {
   address$= this.addressSubject.asObservable();
   ethBalance$= this.ethBalanceSubject.asObservable();
   
-  latestBlock = 0;
+  
   tokenBalance!: TokenBalancesResponseErc20;
   nfts!:OwnedNftsResponse;
-
-
-
-  config = {
-    apiKey: "rO-vFS4QN5qxsKmhgCT24qRXcKYs6Xl_",
-    network:  Network.ETH_SEPOLIA ,
-  };
-
-  
-
-
-  alchemy = new Alchemy(this.config);
   ethBalance!: ethers.BigNumber;
   
   constructor() { 
     this.connectWalletWithAlchemy();
-     
-  }
-  
-  getBlockNumber(): Observable<number> {
-    return from(this.alchemy.core.getBlockNumber());
-  }
-  
-  
-
-
-  async alchemyInit(address: string) { 
-    this.latestBlock =  await this.alchemy.core.getBlockNumber();
-
-    const data = await this.alchemy.core.getAssetTransfers({
-      fromBlock: "0x0",
-      fromAddress: address,
-      category: [AssetTransfersCategory.EXTERNAL, AssetTransfersCategory.INTERNAL, AssetTransfersCategory.ERC20, AssetTransfersCategory.ERC721, AssetTransfersCategory.ERC1155, ],
-    });
-
-    this.nfts = await this.alchemy.nft.getNftsForOwner(address);
-    this.tokenBalance =  await this.alchemy.core.getTokenBalances(address)
-    // alchemy get eth balance
-    this.ethBalance = await this.alchemy.core.getBalance(address)
-
-    console.log(this.latestBlock )
-    console.log(this.nfts);
-    console.log(data);
-    console.log(this.tokenBalance);
-    console.log(this.ethBalance);
   }
 
-  async  logs() { // for contract
-    const getLogs = await this.alchemy.core.getLogs({
-        address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-        topics: [
-          "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-        ],
-        blockHash:
-          "0x49664d1de6b3915d7e6fa297ff4b3d1c5328b8ecf2ff0eefb912a4dc5f6ad4a0",
-    });
-    console.log(getLogs);
-  }
   
   async connectWalletWithEthers()  {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
